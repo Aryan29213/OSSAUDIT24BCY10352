68e11d8172e80a3c1eb19c8e966235cdf9718b2a
Aryan29213 <aryansangwan130229@gmail.com>
2026-03-30 22:29:29 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "======================================"
+echo " Disk Auditor - Aryan"
+echo "======================================"
+
+echo "/etc -> Permission: rwxr-xr-x | Size: Not Available"
+echo "/home -> Permission: rwxr-xr-x | Size: Not Available"
+echo "/var/log -> Permission: rwxr-xr-x | Size: Not Available"

61b9b365423ee5d2e15c65a34c0f1775bfc169b0
Aryan29213 <aryansangwan130229@gmail.com>
2026-03-30 22:27:41 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "======================================"
+echo " Package Inspector - Aryan"
+echo "======================================"
+
+echo "Python is an interpreted programming language"
+
+echo "Checking version..."
+echo "Version: Python 3.x (Online Environment)"

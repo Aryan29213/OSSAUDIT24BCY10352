67b9aa4cdeca04c5c6e8c78795e57643b41a81f7
Aryan29213 <aryansangwan130229@gmail.com>
2026-03-30 22:32:13 +0530

    Create script5.sh

---
+++ script5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "======================================"
+echo " Open Source Manifesto - Aryan"
+echo "======================================"
+
+echo "I use Python daily."
+echo "Freedom means open access to software."
+echo "I want to build useful tools and share them."

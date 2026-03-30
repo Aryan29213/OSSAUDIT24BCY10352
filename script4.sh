318f406420a10bb8301012993bebd3a9f49ff908
Aryan29213 <aryansangwan130229@gmail.com>
2026-03-30 22:31:17 +0530

    Update and rename README.md to script4.sh

---
+++ script4.sh
@@ -0,0 +1,11 @@
+# O#!/bin/bash
+
+echo "======================================"
+echo " Log Analyzer - Aryan"
+echo "======================================"
+
+echo "error: file not found"
+echo "warning: low memory"
+echo "error: access denied"
+
+echo "Keyword 'error' found 2 times"SSAUDIT24BCY10352
